bf66a55fcb3c8c56f89ae163b1f3735d237311e7
Jessica-Arg <jessica.arguello@globant.com>
2022-08-24 16:29:56 -0300

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+// Importar sin el archivo barril de index
+// import { AddCategory } from './components/AddCategory';
+// import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+const [categories, setCategories] = useState(['One Punch']);
+
+const onAddCategory = ( newCategory ) => {
+    // Valorant
+    //Forma 1
+    //categories.push(newCategory);
+    if ( categories.includes(newCategory)) return;
+
+    setCategories([newCategory,...categories]);
+
+}
+
+
+  return (
+    <>
+
+        <h1>GifExpertApp</h1>
+
+        <AddCategory 
+            // setCategories={setCategories}
+            onNewCategory={ value => onAddCategory(value) }
+        />
+
+        <button onClick={onAddCategory}>Agregar</button>
+
+        {
+            categories.map(( category) => (
+                <GifGrid 
+                        key={category} 
+                        category={category}
+                    />
+                ))
+        }
+
+    </>
+  )
+}
